8b437e8013de57efb88152ee5a444cf6abc32695
mancap314 <egregius314@gmail.com>
2018-02-07 23:09:14 +0100

    initial weight scaling introduced

--- mini-nn/toy1.py
+++ mini-nn/toy1.py
@@ -4,6 +4,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
+np.random.seed(1) #set seed (for reproducibility)
+
+
 def nonlin(x, deriv=False):
     '''sigmoid function'''
     if deriv:
@@ -12,7 +15,7 @@ def nonlin(x, deriv=False):
 
 
 def lin(x, deriv=False):
-    ''' linear function'''
+    '''linear function'''
     is_in = (np.abs(x) <= 1).astype(float)
     if deriv:
         return is_in
@@ -22,7 +25,7 @@ def lin(x, deriv=False):
 def plot_error(sq_errs):
     lgds = []
     for name, sq_err in sq_errs.items():
-        line = plt.plot(sq_err, label=name)
+        plt.plot(sq_err, label=name)
         lgds.append(name)
     plt.legend(lgds)
     plt.ylabel('Squared Error')
@@ -32,13 +35,10 @@ def plot_error(sq_errs):
     plt.show()
 
 
-
-# set seed (for reproducibility)
-np.random.seed(1)
-
-def train_1(X, y, activation_function, n_epoch=1000):
+def train_1(X, y, activation_function, n_epoch=1000, initial_weights_scaling=0.1):
+    '''train a 1-layer nn'''
     # initialize weights
-    syn0 = 2 * np.random.random((3, 1)) - 1 #1st term in (0, 2), -1: in (-1, 1), mean: 0
+    syn0 = initial_weights_scaling * np.random.random((3, 1)) - initial_weights_scaling / 2 #1st term in (0, 2), -1: in (-1, 1), mean: 0
     sq_err = []
     for i in range(n_epoch):
          l1 = activation_function(np.dot(X, syn0)) #forward propagation
@@ -51,8 +51,9 @@ def train_1(X, y, activation_function, n_epoch=1000):
     return l1, sq_err
 
 
-def train_2(X, y, activation_function, n_epoch=60000):
-    syn0 = 2 * np.random.random((3, 4)) - 1
+def train_2(X, y, activation_function, n_epoch=60000, initial_weights_scaling=0.1):
+    '''train a 2-layer nn'''
+    syn0 = initial_weights_scaling * np.random.random((3, 4)) - initial_weights_scaling / 2
     syn1 = 2 * np.random.random((4, 1)) - 1
     sq_err = []
     for i in range(n_epoch):  # push forward
@@ -76,7 +77,7 @@ def train_2(X, y, activation_function, n_epoch=60000):
     return l2, sq_err
 
 
-def run_training(X, y, train, n_epoch=1000):
+def run_training(X, y, train, n_epoch=1000, initial_weights_scaling=0.5):
     activation_functions = {'non-linear': nonlin, 'linear':  lin}
     sq_errs = {}
     for name, activation_function in activation_functions.items():
@@ -87,20 +88,23 @@ def run_training(X, y, train, n_epoch=1000):
     plot_error(sq_errs)
 
 
-def run_example_1():
+def run_example_1(n_epoch=1000, initial_weights_scaling=0.5):
     X = np.array([[0, 0, 1],
                       [0, 1, 1],
                       [1, 0, 1],
                       [1, 0, 1]])
     y = np.array([[0, 0, 1, 1]]).T
-    run_training(X, y, train_1, 1000)
+    run_training(X, y, train_1, n_epoch, initial_weights_scaling)
 
 
-def run_example_2():
+def run_example_2(n_epoch=1000, initial_weights_scaling=0.5):
     X = np.array([[0, 0, 1],
                   [0, 1, 1],
                   [1, 0, 1],
                   [1, 1, 1]])
 
     y = np.array([[0, 1, 1, 0]]).T
-    run_training(X, y, train_2, 60000)
+    run_training(X, y, train_2, n_epoch, initial_weights_scaling)
+
+run_example_1()
+run_example_2(n_epoch=60000, initial_weights_scaling=0.01)
